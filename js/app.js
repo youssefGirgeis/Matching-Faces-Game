304e6a037d66ae81acb1a7d918a34eeb76834188
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-09-16 00:37:46 -0400

    stylesheet added

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var leftSide;
+var rightSide;
+var numberOfFaces;
+var image;
+
+
+function generateFaces(){
+    leftSide = document.getElementById('left-div');
+    numberOfFaces = 5;
+    
+    for(var i=0; i<numberOfFaces; i++){
+        image = document.createElement('img');
+        image.setAttribute('src', 'images/smile.png');
+        var top = Math.floor((Math.random() * 401));
+        var left = Math.floor((Math.random() * 401));
+        leftSide.appendChild(image)
+    }
+}
